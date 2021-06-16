61c28640a8e7e6a64dc3ffd5594ffbc5b6b2cd3c
Martin Hirzel <hirzel@gmail.com>
2021-06-15 21:18:11 -0400

    Expanded tests and fixed a couple of problems identified by valgrind.

--- cpp/src/bulk_evict_test.cc
+++ cpp/src/bulk_evict_test.cc
@@ -2,6 +2,23 @@
 #include "AggregationFunctions.hpp"
 
 template<int minArity>
+bool checkContents(btree::Aggregate<int, minArity, btree::finger, Collect<int>>* tree, int minTime, int maxTime) {
+  auto collected = tree->query();
+  int pred = minTime;
+  for (auto j=collected.begin(); j!=collected.end(); j++) {
+    if (pred + 1 != *j) {
+	std::cerr << "collected items ";
+	for (auto k=collected.begin(); k!=collected.end(); k++)
+	  std::cerr << *k << " ";
+	std::cerr << std::endl << "do not match tree " << *tree;
+	return false;
+    }
+    pred = *j;
+  }
+  return pred == maxTime;
+}
+
+template<int minArity>
 void test_one(int height, int iteration, bool verbose) {
   typedef btree::Aggregate<int, minArity, btree::finger, Collect<int>> Tree;
   auto tree = Tree::makeRandomTree(Collect<int>(), height);
@@ -10,19 +27,19 @@ void test_one(int height, int iteration, bool verbose) {
   if (verbose) std::cout << "iteration " << iteration << ", minTime " << minTime << ", maxTime " << maxTime << std::endl;
   if (verbose) std::cout << *tree;
   tree->evictUpTo(minTime);
-  auto collected = tree->query();
-  int pred = minTime;
-  for (auto j=collected.begin(); j!=collected.end(); j++) {
-    if (pred + 1 != *j) {
-	std::cerr << "iteration " << iteration << " collected items and tree ";
-	for (auto k=collected.begin(); k!=collected.end(); k++)
-	  std::cerr << *k << " ";
-	std::cerr << std::endl << *tree;
-	assert(false);
-    }
-    pred = *j;
+  if (!checkContents(tree, minTime, maxTime)) {
+    std::cerr << "iteration " << iteration << " failed evictUpTo" << std::endl;
+    assert(false);
   }
-  assert(pred == maxTime);
+  if (iteration < 3) {
+    if (true) std::cout << "height " << height << ", iteration " << iteration << ", minTime " << minTime << ", maxTime " << maxTime << std::endl;
+    for (int i=1+maxTime, n=2*maxTime; i<=n; i++)
+      tree->insert(i, i);
+    if (!checkContents(tree, minTime, 2*maxTime)) {
+      std::cerr << "iteration " << iteration << " failed inserts" << std::endl;
+      assert(false);
+    }
+  }
   delete tree;
 }
 
