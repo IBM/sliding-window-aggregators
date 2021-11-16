7c0e8117cebfe49aa1cfcd01121239767ec361af
Kanat Tangwongsan <kanat.tan@mahidol.edu>
2021-11-16 16:47:26 +0700

    Complete AMTA with insert, evict, and query
    (No bulkEvict just yet)

--- cpp/src/AMTA.hpp
+++ cpp/src/AMTA.hpp
@@ -7,6 +7,9 @@
 #include <vector>
 #include <iterator>
 
+
+// BUG: evicting size 1
+
 namespace amta {
 
 using namespace std;
@@ -27,10 +30,17 @@ private:
     int arity;
     Node(aggT a, Node* left) : parent(NULL), arity(0) { push_back(a, left); }
     void push_back(aggT a, Node* child) {
+      assert (arity == 0 || arity == 1);
       agg[arity] = a; children[arity] = child; arity++;
       if (child != NULL) child->parent = this;
     }
-    void pop_front() { arity = -1; }
+    void pop_front() {
+      assert(arity != 0);
+      if (abs(arity) == 1)
+        arity = 0;
+      else
+        arity = -1;
+    }
     bool leftEmpty() { return arity == -1; }
     bool rightEmpty() { return arity == 1; }
     bool full() { return arity == 2; }
@@ -38,6 +48,8 @@ private:
     ostream& printNode(ostream& os) const {
       os << "[";
       for (int i=0;i<arity;i++) os << this->agg[i] << ",";
+      if (arity == -1)
+        os << "-, " << this->agg[1];
       os << "]";
       return os;
     }
@@ -50,13 +62,15 @@ private:
   binOpFunc _binOp;
   size_t _size;
   vector<Node *> _tails;
+  Node* _frontNode; // ptr to the node storing the oldest elt
   deque<aggT> _frontStack;
   aggT _identE, _frontSum, _backSum;
 
 public:
   Aggregate(binOpFunc binOp, aggT identE_)
-    : _binOp(binOp), _size(0), _tails(), _frontStack(),
-      _identE(identE_), _frontSum(identE_), _backSum(identE_) {}
+      : _binOp(binOp), _size(0), _tails(), _frontNode(NULL),
+        _frontStack(), _identE(identE_), _frontSum(identE_), _backSum(identE_) {
+  }
 
   ~Aggregate() {
       // TODO: write me
@@ -70,40 +84,72 @@ public:
     std::cout << std::endl;
     std::cout << "_frontSum=" << _frontSum << ", _backSum=" << _backSum
               << std::endl;
+    std::cout << "_frontStack:";
+    for (auto elt: _frontStack) { std::cout << elt << ", "; }
+    std::cout << std::endl;
+    std::cout << "_frontNode:" << *_frontNode << std::endl;
+  }
+  void rebuildFrontFrom(Node *c) {
+    aggT agg = _frontStack.empty()?_identE:_frontStack.back();
+    while (c != NULL) {
+      Node *next = c->leftEmpty() ? c->children[1] : c->children[0];
+      if (c->full()) {
+        agg = _binOp.combine(c->agg[1], agg);
+        _frontStack.push_back(agg);
+      }
+      if (next == NULL) // reset the frontNode pointer
+        _frontNode = c;
+      c = next;
+    }
   }
   void rebuildFront() {
     if (_tails.empty()) { _frontSum = _identE; return ;}
+    _frontStack.clear();
 
-    Node* c =  _tails.back();
-    aggT agg = _identE;
-    while (c != NULL) {
-      Node* next = c->leftEmpty()?c->children[1]:c->children[0];
-      if (c->full()) {
-        agg = _binOp.combine(c->agg[1], agg);;
-        _frontStack.push_back(agg);
-        if (next == NULL) {
-          aggT other = c->full()?c->agg[0]:c->agg[1];
-          agg = _binOp.combine(other, agg);
-        }
-      }
-      c = next;
-    }
-    _frontSum = agg;
+    rebuildFrontFrom(_tails.back());
+    aggT agg = _frontStack.empty() ? _identE : _frontStack.back();
+    aggT other = (_frontNode->full() || _frontNode->rightEmpty())
+                     ? _frontNode->agg[0]
+                     : _frontNode->agg[1];
+    _frontSum = _binOp.combine(other, agg);
   }
 
   void rebuildBack() {
+    if (_tails.empty()) { _backSum = _identE; return ;}
     aggT agg = _identE;
     auto it = _tails.rbegin();
 
     while (++it != _tails.rend()) {
       Node *c = *it;
-      aggT nodeAgg = c->full()?_binOp.combine(c->agg[0], c->agg[1]):c->agg[0];
+      aggT nodeAgg =
+          c->full() ? _binOp.combine(c->agg[0], c->agg[1]) : c->agg[0];
       agg = _binOp.combine(agg, nodeAgg);
     }
     _backSum = agg;
   }
 
-  void evict() { throw 1; }
+  void evict() {
+    _frontSum = _frontStack.empty() ? _identE : _frontStack.back();
+    if (_size > 0) _size--;
+    Node *c = _frontNode;
+    while (c != NULL) {
+      if (c->full()) _frontStack.pop_back(); // was a full node, no longer
+      c->pop_front();
+      if (c->arity != 0)
+        break;
+      Node *next = c->parent;
+      delete c;
+      c = next;
+    }
+    if (c == NULL) { // hit the big root
+//      std::cout << "big root emptied, moving on..." << std::endl;
+      _tails.pop_back();
+      _frontNode = NULL;
+      rebuildFront(), rebuildBack();
+    }
+    else
+      rebuildFrontFrom(c);
+  }
   void bulkEvict() { throw 1; }
   void bulkInsert(vector<pair<timeT, inT>> entries) {
     bulkInsert(entries.begin(), entries.end());
@@ -117,24 +163,34 @@ public:
       }
   }
 
-  timeT oldest() const { throw 1; }
+  timeT oldest() const {
+    return _frontNode->leftEmpty() ? _frontNode->latestTime[1]
+                                   : _frontNode->latestTime[0];
+  }
 
-  outT query() const { return _binOp.combine(_frontSum, _backSum); }
+  outT query() const {
+    return _binOp.lower(_binOp.combine(_frontSum, _backSum));
+  }
 
   size_t size() { return _size; }
 
-  timeT youngest() const { throw 1; }
+  timeT youngest() const {
+    Node *backNode = _tails.front();
+    return backNode->full() ? backNode->latestTime[1] : backNode->latestTime[0];
+  }
 
   void insert_lifted(timeT const& time, aggT const& liftedValue) {
     bool hasCarry = true;
-    Node* carriedFrom = NULL;
+    Node *carriedFrom = NULL;
     aggT carry = liftedValue;
     bool bigRootHit = false;
     _backSum = _binOp.combine(_backSum, liftedValue);
-    for (auto it=_tails.begin(); it != _tails.end(); it++) {
+    for (auto it = _tails.begin(); it != _tails.end(); it++) {
       Node *node = *it;
-      if (node->arity == 2) {
-        auto nextCarry = _binOp.combine(node->agg[0], node->agg[1]);
+      if (node->full() || node->leftEmpty()) { // has room for carry
+        auto nextCarry = node->full()
+                             ? _binOp.combine(node->agg[0], node->agg[1])
+                             : node->agg[1];
         *it = new Node(carry, carriedFrom); // with just carry
         carriedFrom = node, carry = nextCarry;
       } else { // found a non-full node
@@ -145,9 +201,12 @@ public:
         break;
       }
     }
-    if (hasCarry)
-      _tails.push_back(new Node(carry, carriedFrom));
-    else if (bigRootHit) {
+    if (hasCarry) {
+      Node *n = new Node(carry, carriedFrom);
+      if (_tails.empty())
+        _frontNode = n;
+      _tails.push_back(n);
+    } else if (bigRootHit) {
       rebuildFront(), rebuildBack();
     }
   }
@@ -163,6 +222,7 @@ public:
       timeT const time = 1 + youngest();
       insert(time, val);
     }
+    _size++;
   }
 };
 
--- cpp/src/amta_test.cc
+++ cpp/src/amta_test.cc
@@ -5,11 +5,20 @@ using namespace std;
 int main(int argc, char *argv[]) {
     auto f = Sum<int>();
     amta::Aggregate agg = amta::make_aggregate<long>(f, 0);
-
-    for (int i=0;i<20;i++) {
+    int n=5;
+    for (int i=0;i<n;i++) {
         agg.insert(i, i+1);
         agg.print();
         cout << agg.query() << endl;
     }
+
+    for (int i=0;i<n-1;i++) {
+      agg.evict();
+      agg.print();
+      cout << agg.query() << endl;
+    }
+    agg.insert(n+1, 7);
+    agg.print();
+    cout << agg.query() << endl;
     return 0;
 }
