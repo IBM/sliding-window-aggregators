775508e0dff1ffa309a64a34c9fc445bff17ae16
Kanat Tangwongsan <kanat.tan@mahidol.edu>
2022-03-15 17:06:48 +0700

    Update underlying OS and add bulk_* to the build
    * Update the build image to Ubuntu 20.04
    * Include bulk benchmarks in the build script

--- cpp/build-and-test.sh
+++ cpp/build-and-test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-TARGETS="benchmark_driver benchmark_driver_stats ooo_benchmark data_benchmark test dynamic_benchmark"
+TARGETS="benchmark_driver benchmark_driver_stats ooo_benchmark data_benchmark test dynamic_benchmark bulk_evict_benchmark bulk_evict_insert_benchmark"
 IMAGE="swag-builder-cpp"
 CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # only proceed if this directory is in the latest commit
+cd ${CWD}/..
 if $(cpp/can-skip.sh cpp); then
     echo "Skipping build and test for C++ code."
     exit 0
